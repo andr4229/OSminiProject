db9b7fe48d22c3c3b84bcc02b239b102dd5257fa
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2019-03-20 10:34:45 +0100

    Create Slideshow.java

---
+++ ImageViewer_Startup-master/src/imageviewerproject/Slideshow.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageviewerproject;
+
+import java.util.List;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author Wezzy Laptop
+ */
+public class Slideshow {
+    
+    
+    
+    
+    static int nextSlide(int count)
+    {
+        
+        return count++;
+    }
+    
+}
